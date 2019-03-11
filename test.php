67bd69496fc174c1607e5ccf3954665cdd6f1cc9
wahyumeka <mekawahyu18@gmail.com>
2019-03-11 09:52:57 +0700

    menambahkan line baru

--- test.php
+++ test.php
@@ -1,9 +1,13 @@
 <?php
 
 echo "ini adalah test";
+echo "<br>";
+
 echo "ini adalah test again";
+echo "<br>";
+
 echo "tes lagi aja";
-echo "<br>"
+echo "<br>";
 echo "<h1 style='color:#43B7D2;'front-family:verdana;'>this is a blue Heading</h1>";
 
 
